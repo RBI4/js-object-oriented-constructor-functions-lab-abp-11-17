f6b3988212f13cf3c874a1f150b45797f2e59ef8
RBI4 <rebecca.inge@gmail.com>
2017-11-14 18:38:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,3 +4,12 @@ function Scooter(year, color, model){
   this.model = model
 }
   let samson = new Scooter(2015, "red", "swoosh")
+
+function Driver(name, age, experience){
+  this.name = name
+  this.age = age
+  this.experience = experience
+}
+
+let allison = new Driver("Allison", "16 years", "2 years")
+let this.experience=2
